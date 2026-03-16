45b839d988ca62702f00b4a4c592c3cecfae4eaa
delta student <mk0925256@gmail.com>
2026-03-16 20:58:06 +0530

    fix

--- server/config/openRouter.js
+++ server/config/openRouter.js
@@ -1,40 +1,4 @@
-// const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions"
-// const model = "deepseek/deepseek-chat"
 
-// const generateResponse = async (prompt) => {
-//     const res = await fetch(openRouterUrl, {
-//   method: 'POST',
-//   headers: {
-//     Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-//    // Optional. Site URL for rankings on openrouter.ai.
-//   // Optional. Site title for rankings on openrouter.ai.
-//     'Content-Type': 'application/json',
-//   },
-//  body: JSON.stringify({
-//   model: model,
-//   messages: [
-//     { role: "system", content: "You must return ONLY valid raw JSON." },
-//     {
-//       role: "user",
-//       content: prompt,
-//     },
-//   ],
-//   temperature:0.2
-// }),
-
-// });
-
-
-// if(!res.ok){
-//   const err = await res.text()
-//   throw new Error("openRouter err"+err)
-// }
-
-// const data = await res.json()
-// return data.choices[0].message.content
-// }
-
-// export default generateResponse
 
 const openRouterUrl = "https://openrouter.ai/api/v1/chat/completions";
 const model = "deepseek/deepseek-chat";
